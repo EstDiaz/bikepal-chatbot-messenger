a8ef7f1d47e8e66cd167f4b3907ca706677613c9
Esteban Díaz <esteban.diaz@finect.com>
2017-10-07 12:09:03 +0200

    log

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
   var data = req.body;
   console.log('>>> MESSAGE RECEIVED');
-  console.log('data');
+  console.log(data);
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
